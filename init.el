577cb86f3fd387ab15bdae56b2e9a53691363947
Rajat Mhetre <mhetrerajat@gmail.com>
2017-08-27 02:51:55 +0530

    sublime themes added

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 ;; INSTALL PACKAGES
 ;; --------------------------------------
 
+
 (require 'package)
 
 (add-to-list 'package-archives
@@ -14,6 +15,7 @@
 
 (defvar myPackages
   '(better-defaults
+    elpy
     material-theme))
 
 (mapc #'(lambda (package)
@@ -24,8 +26,28 @@
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
 
+;; enable elpy
+(elpy-enable)
+
 ;; (setq inhibit-startup-message t) ;; hide the startup message
 (load-theme 'material t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
 
 ;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" default)))
+ '(package-selected-packages
+   (quote
+    (sublime-themes material-theme better-defaults dict-tree coffee-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil)))))
