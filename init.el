c648dcef1e61416bec19845009e51b44a954e9c3
Rajat Mhetre <mhetrerajat@gmail.com>
2017-08-27 22:08:13 +0530

    minor modifications to disable GUI

--- init.el
+++ init.el
@@ -49,6 +49,33 @@
 (global-set-key (kbd "C-M-k") 'scroll-down-line)
 
 
+;; GUI specific settings
+;; Load the customizations after an emacsclient startsup.
+(defun disable-crappy-frames (&optional frame)
+  "Disables scrollbars, toolbars and fringe while in graphical mode."
+  (when (or window-system frame)
+    ;; Highlighting current line
+    (global-hl-line-mode 1)
+
+    ;; (set-face-italic-p 'italic nil)
+
+    ;; Disabling the fringe
+    (set-fringe-mode '(0 . 0))
+
+    ;; Disable the scrollbar
+    (scroll-bar-mode -1)
+
+    ;; Setting the default font
+    ;; (set-default-font "Liberation Mono 12")
+    ;;(add-to-list 'default-frame-alist '(font . "Source Code Pro-14"))
+    ;;(set-face-attribute 'default nil :height 150)
+    ;;(set-face-attribute 'default nil :font "Liberation Mono 9")
+
+    ;; Disable the toolbar
+    (tool-bar-mode -1)))
+
+(disable-crappy-frames)
+
 
 ;; INSTALL PACKAGES
 ;; --------------------------------------
@@ -97,6 +124,7 @@
 (load "nyan-mode.el")
 (nyan-mode 1)
 (nyan-start-animation)
+
 ;; Custom modeline for nyan mode
 (setq mode-line-format
       (list
@@ -127,6 +155,10 @@
 ;; Revert current hunk
 (global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
 
+;; Disable all extras of GUI.
+(add-hook 'server-visit-hook 'disable-crappy-frames)
+(add-hook 'after-make-frame-functions 'disable-crappy-frames)
+
 
 ;; To beautify JSON
 (defun json-format ()
@@ -146,7 +178,7 @@
     ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" default)))
  '(package-selected-packages
    (quote
-    (restart-emacs sublime-themes material-theme better-defaults dict-tree coffee-mode))))
+    (## restart-emacs sublime-themes material-theme better-defaults dict-tree coffee-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
