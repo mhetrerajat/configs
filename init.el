9902a22cd7d1181245db9789afe00961d683f303
Rajat Mhetre <mhetrerajat@gmail.com>
2017-08-27 21:02:36 +0530

    nyan mode and git gutter mode added

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 ;; init.el --- Emacs configuration
 
+;; Disable splash screen
+(setq inhibit-startup-message t)
+
+;; Disabling bold fonts
+(set-face-bold-p 'bold nil)
+
 ;; utf-8
 (prefer-coding-system 'utf-8)
 (setq coding-system-for-read 'utf-8)
@@ -83,6 +89,37 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 
+;; Nyan mode
+(add-to-list 'load-path "plugins/nyan-mode")
+(load "nyan-mode.el")
+(nyan-mode 1)
+(nyan-start-animation)
+
+;; git gutter mode
+(add-to-list 'load-path "plugins/git-gutter")
+(load "git-gutter.el")
+
+;; If you enable global minor mode
+(global-git-gutter-mode t)
+
+;; If you would like to use git-gutter.el and linum-mode
+(git-gutter:linum-setup)
+
+(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+
+;; Jump to next/previous hunk
+;;(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+
+;; Stage current hunk
+(global-set-key (kbd "C-x g s") 'git-gutter:stage-hunk)
+
+;; Revert current hunk
+(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -93,7 +130,7 @@
     ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" default)))
  '(package-selected-packages
    (quote
-    (sublime-themes material-theme better-defaults dict-tree coffee-mode))))
+    (restart-emacs sublime-themes material-theme better-defaults dict-tree coffee-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
