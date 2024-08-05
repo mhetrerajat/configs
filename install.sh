f922232cc822654911c867d7cead47053de496c6
Rajat Mhetre <mhetrerajat@gmail.com>
2024-08-04 19:50:26 -0700

    add git specific formula

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ main() {
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
+	# GIT
+	brew install git gpg
+
+
 	# install neovim
 	brew install neovim
 
